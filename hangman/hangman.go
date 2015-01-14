0de2741643ea891b652659d125809b5f52fd8df8
bdowns <brian.downs@gmail.com>
2015-01-14 08:37:46 -0700

    small change

--- hangman/hangman.go
+++ hangman/hangman.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Simple implementation of hangman
+// An implementation of hangman
 package main
 
 import (
